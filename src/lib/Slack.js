b33ce82b8d3ec3c54728e9c0f60fdbf14d436e05
Christiaan Mealey <chris.mealey@gmail.com>
2017-09-22 09:25:46 -0400

    the dude, man

--- src/lib/Slack.js
+++ src/lib/Slack.js
@@ -1,11 +1,17 @@
 let rickJames = require('./RickJames');
-
+let theDude = require('./TheDude');
 let Slack = {
   getSlides: () => {
     return rickJames.getChannelMessages();
   },
   getUserList: () => {
     return rickJames.getUserList();
+  },
+  getDudeQuotes: () => {
+    return theDude.getQuotes();
+  },
+  getCats: () => {
+    return theDude.getCats();
   }
 };
 
