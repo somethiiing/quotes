af778ae9b0ab4c3d5996c68b5d905a680799cc59
Christiaan Mealey <chris.mealey@gmail.com>
2017-09-22 10:21:51 -0400

    mongo wip

--- src/lib/BackFires.js
+++ src/lib/BackFires.js
@@ -1,22 +1,52 @@
-let mongo = require('mongo').MongoClient,
-connectionString = 'mongodb://rickjames:rickjames123@ds141534.mlab.com:41534/rickjamessecretstash';
+let MongoClient = require('mongodb').MongoClient;
+let connectionString = 'mongodb://rickjames:rickjames123@ds141534.mlab.com:41534/rickjamessecretstash';
 
 let Data = {
   dbInstance: null,
+  getDbInstance: function() {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      if (!this.dbInstance) {
+        MongoClient.connect(connectionString).then((db) => {
+          this.dbInstance = db;
+          resolve(this.dbInstance);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      }
+    });
+  },
   getImages: function () {
-    if (!this.dbInstance) {
-      mongo.connect(connectionString).then((db) => {
-        return db.collection('randomcarimages').find({}, {'url': 1});
-      });
-    }
+    return this.getDbInstance().then((db) => {
+      return db.collection('randomcarimages').find({}, {'url': 1});
+    })
+    .then((images) => {
+      return images.toArray();
+    })
+    .then((images) => {
+      return images;
+    });
   },
   getComments: function () {
-    if (!this.dbInstance) {
-      mongo.connect(connectionString).then((db) => {
-        return db.collection('randombackfirescomments').find({});
-      });
-    }
+    return this.getDbInstance().then((db) => {
+      return db.collection('randombackfirescomments').find({});
+    })
+    .then((comments) => {
+      return comments.toArray();
+    })
+    .then((comments) => {
+      return comments;
+    });
   }
 };
-console.log(Data.getImages());
+/*
+Data.getImages().then((res) => {
+  console.log(res);
+});
+
+Data.getComments().then((res) => {
+  console.log(res);
+});
+*/ 
 module.exports = Data;
